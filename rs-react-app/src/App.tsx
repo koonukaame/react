8c63c0897966885c48773eac3639a641f2a571ca
koonukaame <marialezhebokova@yandex.ru>
2025-06-28 21:46:12 +0300

    chore: install tailwind

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -10,14 +10,22 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img
+            src={viteLogo}
+            className="h-24 p-6 will-change-filter transition-filter duration-300 hover:drop-shadow-[0_0_2em_#646cffaa]"
+            alt="Vite logo"
+          />
         </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img
+            src={reactLogo}
+            className="h-24 p-6 will-change-filter transition-filter duration-300 react:hover:drop-shadow-[0_0_2em_#61dafbaa]"
+            alt="React logo"
+          />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="p-8">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -25,7 +33,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="text-[#888]">
         Click on the Vite and React logos to learn more
       </p>
     </>
