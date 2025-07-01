9b4991c262680919530e0b331cd1f87d1f00625f
koonukaame <marialezhebokova@yandex.ru>
2025-07-01 17:06:55 +0300

    fix: convert formData to URLSearchParams

--- rs-react-app/src/services/startrack.ts
+++ rs-react-app/src/services/startrack.ts
@@ -3,9 +3,15 @@ import type { Conflict } from '../entities';
 export const searchConflict = async (
   body: FormData
 ): Promise<{ conflicts: Conflict[] }> => {
+  const formData = new URLSearchParams();
+
+  for (const [key, value] of body) {
+    formData.append(key, value.toString());
+  }
+
   const response = await fetch(`https://stapi.co/api/v1/rest/conflict/search`, {
     method: 'POST',
-    body,
+    body: formData,
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
     },
