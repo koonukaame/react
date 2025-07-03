66eea1fcc12e0389a234648a3702986e369e6206
koonukaame <marialezhebokova@yandex.ru>
2025-07-03 15:41:51 +0300

    feat: add onSubit method

--- rs-react-app/src/components/SearchForm.tsx
+++ rs-react-app/src/components/SearchForm.tsx
@@ -1,4 +1,9 @@
-import { Component, type ChangeEvent, type ReactNode } from 'react';
+import {
+  Component,
+  type ChangeEvent,
+  type FormEvent,
+  type ReactNode,
+} from 'react';
 
 type Props = {
   onSearch: (data: FormData) => void;
@@ -15,6 +20,7 @@ export class SearchForm extends Component<Props, State> {
     super(props);
     this._onSearch = props.onSearch;
     this._onChange = this._onChange.bind(this);
+    this._onSubmit = this._onSubmit.bind(this);
 
     const initInputValue = localStorage.getItem(searchKey) ?? '';
 
@@ -25,7 +31,7 @@ export class SearchForm extends Component<Props, State> {
 
   render(): ReactNode {
     return (
-      <form action={this._onSearch}>
+      <form onSubmit={this._onSubmit}>
         <input
           value={this.state.input}
           onChange={this._onChange}
@@ -37,12 +43,20 @@ export class SearchForm extends Component<Props, State> {
     );
   }
 
-  private _onSearch: Props['onSearch'] = () => {};
-  private _onChange = (e: ChangeEvent<HTMLInputElement>) => {
+  private _onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    this._onSearch(formData);
+  }
+
+  private _onChange(e: ChangeEvent<HTMLInputElement>) {
     localStorage.setItem(searchKey, e.target.value);
 
     this.setState({
       input: e.target.value,
     });
-  };
+  }
+
+  private _onSearch: Props['onSearch'];
 }
