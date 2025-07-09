b648051f0e5de831d7f60e9a56ca5cc74ab00714
koonukaame <marialezhebokova@yandex.ru>
2025-07-09 12:27:54 +0300

    refactor: change CharsNotFound to MsgBlock

--- rs-react-app/src/components/CharsResult.tsx
+++ rs-react-app/src/components/CharsResult.tsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import type { Character } from '../entities';
-import { CharsTable, CharsNotFound } from './index';
+import { CharsTable, MsgBlock } from './index';
 
 type Props = {
   characters: Character[];
@@ -9,7 +9,12 @@ type Props = {
 export class CharsResult extends Component<Props> {
   render() {
     if (this.props.characters.length === 0) {
-      return <CharsNotFound />;
+      return (
+        <MsgBlock
+          title="No characters found 🕵️‍♀️"
+          msg="Try adjusting your search, maybe a typo snuck in?"
+        />
+      );
     }
 
     return <CharsTable characters={this.props.characters} />;
