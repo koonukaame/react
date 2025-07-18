49a3ec2af4d8ee388d7aa25379c5c72172e8e88a
koonukaame <marialezhebokova@yandex.ru>
2025-07-18 15:03:11 +0300

    test: test Spinner component

---
+++ rs-react-app/src/components/Spinner.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { describe, it, expect } from 'vitest';
+import { Spinner } from './Spinner';
+
+describe('Spinner', () => {
+  it('renders without errors', () => {
+    render(<Spinner></Spinner>);
+    const spinner = screen.getByTestId('spinner');
+    expect(spinner).toBeInTheDocument();
+  });
+});
--- rs-react-app/src/components/Spinner.tsx
+++ rs-react-app/src/components/Spinner.tsx
@@ -1,9 +1,12 @@
-import { Component } from 'react';
+import { Component, type ReactNode } from 'react';
 
 export class Spinner extends Component {
-  render() {
+  render(): ReactNode {
     return (
-      <div className="fixed inset-0 bg-black/20 flex justify-center items-center">
+      <div
+        className="fixed inset-0 bg-black/20 flex justify-center items-center"
+        data-testid="spinner"
+      >
         <div className="w-10 h-10 border-4 border-rose-300 border-t-rose-500 rounded-full animate-spin" />
       </div>
     );
