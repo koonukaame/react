ac079dd33c48c89fb61341a2b8612ebb6f4438ad
koonukaame <marialezhebokova@yandex.ru>
2025-07-18 15:19:42 +0300

    test: MsgBlock component

---
+++ rs-react-app/src/components/MsgBlock.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { describe, it, expect } from 'vitest';
+import { MsgBlock, type Props } from './MsgBlock';
+
+describe('Message block', () => {
+  it('renders without errors', () => {
+    const mockProps: Props = {
+      title: 'test',
+      msg: 'test',
+    };
+    render(<MsgBlock {...mockProps} />);
+    const msgBlock = screen.getByTestId('msg-block');
+    expect(msgBlock).toBeInTheDocument();
+  });
+});
