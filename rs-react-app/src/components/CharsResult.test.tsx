dd45b0375524b904b93289edcdd29c42b99e3d4a
koonukaame <marialezhebokova@yandex.ru>
2025-07-20 18:40:44 +0300

    test: CharsResult component

---
+++ rs-react-app/src/components/CharsResult.test.tsx
@@ -0,0 +1,38 @@
+import { describe, expect, it } from 'vitest';
+import '@testing-library/jest-dom';
+import { CharsResult } from './CharsResult';
+import { CharactersContext } from '../features';
+import { render, screen } from '@testing-library/react';
+import { mockChars } from '../test-utils';
+
+describe('CharsTable component', () => {
+  it('renders MsgBlock when characters array is empty', () => {
+    const mockContext = {
+      characters: [],
+    };
+
+    render(
+      <CharactersContext.Provider value={mockContext}>
+        <CharsResult />
+      </CharactersContext.Provider>
+    );
+
+    expect(screen.getByTestId('msg-block')).toBeInTheDocument();
+    expect(screen.queryByTestId('chars-table')).not.toBeInTheDocument();
+  });
+
+  it('renders CharsTable when characters array is not empty', () => {
+    const mockContext = {
+      characters: mockChars,
+    };
+
+    render(
+      <CharactersContext.Provider value={mockContext}>
+        <CharsResult />
+      </CharactersContext.Provider>
+    );
+
+    expect(screen.queryByTestId('msg-block')).not.toBeInTheDocument();
+    expect(screen.getByTestId('chars-table')).toBeInTheDocument();
+  });
+});
