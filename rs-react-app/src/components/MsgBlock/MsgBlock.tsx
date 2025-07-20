81c56c67a5e2fbdf71aee1074b5a1c6a34b412fc
koonukaame <marialezhebokova@yandex.ru>
2025-07-21 01:18:08 +0300

    refactor: migrate MsgBlock to functional component

--- rs-react-app/src/components/MsgBlock/MsgBlock.tsx
+++ rs-react-app/src/components/MsgBlock/MsgBlock.tsx
@@ -1,20 +1,16 @@
-import { Component, type ReactNode } from 'react';
-
 export type Props = {
   title: string;
   msg: string;
 };
 
-export class MsgBlock extends Component<Props> {
-  render(): ReactNode {
-    return (
-      <div
-        className="text-center text-rose-600 bg-rose-50 py-6 px-4 mt-4"
-        data-testid="msg-block"
-      >
-        <p className="text-lg font-medium mb-2">{this.props.title}</p>
-        <p className="text-sm">{this.props.msg}</p>
-      </div>
-    );
-  }
+export function MsgBlock({ title, msg }: Props) {
+  return (
+    <div
+      className="text-center text-rose-600 bg-rose-50 py-6 px-4 mt-4"
+      data-testid="msg-block"
+    >
+      <p className="text-lg font-medium mb-2">{title}</p>
+      <p className="text-sm">{msg}</p>
+    </div>
+  );
 }
