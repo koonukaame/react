9844b2481a6182d034c68cfa6226549ca6b8f6b3
koonukaame <marialezhebokova@yandex.ru>
2025-07-21 18:30:44 +0300

    refactor: move search key in shared

--- rs-react-app/src/components/SearchForm/SearchForm.tsx
+++ rs-react-app/src/components/SearchForm/SearchForm.tsx
@@ -4,6 +4,7 @@ import {
   type FormEvent,
   type ReactNode,
 } from 'react';
+import { SEARCH_KEY } from '../../shared';
 
 type Props = {
   onSearch: (data: FormData) => void;
@@ -13,8 +14,6 @@ type State = {
   input: string;
 };
 
-const searchKey = 'search';
-
 export class SearchForm extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
@@ -22,7 +21,7 @@ export class SearchForm extends Component<Props, State> {
     this._onChange = this._onChange.bind(this);
     this._onSubmit = this._onSubmit.bind(this);
 
-    const initInputValue = localStorage.getItem(searchKey) ?? '';
+    const initInputValue = localStorage.getItem(SEARCH_KEY) ?? '';
 
     this.state = {
       input: initInputValue,
@@ -65,17 +64,19 @@ export class SearchForm extends Component<Props, State> {
 
   private _onSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    const trimmedInput = this.state.input.trim();
 
-    this.setState({ input: this.state.input.trim() });
+    this.setState({ input: trimmedInput });
+    localStorage.setItem(SEARCH_KEY, trimmedInput);
 
     const formData = new FormData(event.currentTarget);
-    formData.set('name', this.state.input);
+    formData.set('name', trimmedInput);
 
     this._onSearch(formData);
   }
 
   private _onChange(e: ChangeEvent<HTMLInputElement>) {
-    localStorage.setItem(searchKey, e.target.value);
+    localStorage.setItem(SEARCH_KEY, e.target.value);
 
     this.setState({
       input: e.target.value,
