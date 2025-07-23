c44e4deb3ee9dac31a6155542db4acaa08e60c57
koonukaame <marialezhebokova@yandex.ru>
2025-07-23 18:48:11 +0300

    feat: update MsgBlock

--- rs-react-app/src/components/MsgBlock/MsgBlock.tsx
+++ rs-react-app/src/components/MsgBlock/MsgBlock.tsx
@@ -1,12 +1,16 @@
 export type Props = {
   title: string;
   msg: string;
+  isItemDetails?: boolean;
 };
 
-export function MsgBlock({ title, msg }: Props) {
+export function MsgBlock({ title, msg, isItemDetails = false }: Props) {
+  const defaultStyles = 'bg-rose-50 text-rose-800';
+  const detailsStyles = 'w-2/3 bg-white text-gray-800';
+  const baseStyles = 'text-center p-6';
   return (
     <div
-      className="text-center text-rose-600 bg-rose-50 py-6 px-4 mt-4"
+      className={`${isItemDetails ? detailsStyles : defaultStyles} ${baseStyles}`}
       data-testid="msg-block"
     >
       <p className="text-lg font-medium mb-2">{title}</p>
