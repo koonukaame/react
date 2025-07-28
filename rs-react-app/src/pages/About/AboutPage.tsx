de28589b15ae22ab970b0b1ad99126a433529133
koonukaame <marialezhebokova@yandex.ru>
2025-07-28 18:29:33 +0300

    refactor: change a to Link

--- rs-react-app/src/pages/About/AboutPage.tsx
+++ rs-react-app/src/pages/About/AboutPage.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router';
 import { RedirectBtn } from '../../components';
 
 export function About() {
@@ -11,26 +12,26 @@ export function About() {
       </h1>
       <p className="mb-10 text-2xl text-rose-900 max-w-2xl leading-relaxed text-center">
         My GitHub:{' '}
-        <a
+        <Link
           className="font-semibold text-pink-700 underline underline-offset-2 hover:text-pink-900 transition-colors duration-200"
-          href="https://github.com/koonukaame"
+          to="https://github.com/koonukaame"
           target="_blank"
           rel="noreferrer"
         >
           koonukaame
-        </a>
+        </Link>
       </p>
       <RedirectBtn url="/" locationText="Back to Main Page" />
       <p className="absolute bottom-6 text-2xl text-rose-900 max-w-2xl leading-relaxed text-center">
         made in{' '}
-        <a
-          href="https://rs.school/courses/reactjs"
+        <Link
+          to="https://rs.school/courses/reactjs"
           target="_blank"
           rel="noreferrer"
           className="font-semibold text-pink-700 underline underline-offset-2 hover:text-pink-900 transition-colors duration-200"
         >
           Rolling Scopes School
-        </a>
+        </Link>
       </p>
     </main>
   );
