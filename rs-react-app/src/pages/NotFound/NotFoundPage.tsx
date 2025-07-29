66dcf98b7ffa7a4d68c063f8c1fbe87213153ce0
koonukaame <marialezhebokova@yandex.ru>
2025-07-29 12:12:29 +0300

    refactor: NotFound page

--- rs-react-app/src/pages/NotFound/NotFoundPage.tsx
+++ rs-react-app/src/pages/NotFound/NotFoundPage.tsx
@@ -1,9 +1,7 @@
-import { RedirectBtn } from '../../components';
-
 export function NotFound() {
   return (
     <main
-      className="flex flex-col items-center justify-center min-h-screen"
+      className="flex flex-col items-center justify-center min-h-[80vh]"
       data-testid="not-found"
     >
       <h1 className="text-9xl font-extrabold text-rose-700/80 mb-6 tracking-wide">
@@ -12,7 +10,6 @@ export function NotFound() {
       <span className="text-2xl font-semibold text-rose-900 mb-3">
         Page Not Found
       </span>
-      <RedirectBtn url="/" locationText="Back to Main Page" />
     </main>
   );
 }
