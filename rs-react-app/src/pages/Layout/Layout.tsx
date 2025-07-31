409c3f2610a2f296c4560274b1ce73599f73fbf8
koonukaame <marialezhebokova@yandex.ru>
2025-07-31 13:42:39 +0300

    refactor: Layout component

---
+++ rs-react-app/src/pages/Layout/Layout.tsx
@@ -0,0 +1,44 @@
+import { NavLink, Outlet } from 'react-router';
+
+const navigationLinks = [
+  {
+    to: { pathname: '/', search: '?page=1' },
+    text: 'Main',
+  },
+  {
+    to: { pathname: '/about' },
+    text: 'About',
+  },
+];
+
+export function Layout() {
+  return (
+    <div
+      className="flex flex-col min-h-screen max-w overflow-hidden p-6"
+      data-testid="layout"
+    >
+      <header>
+        <nav className="flex gap-6">
+          {navigationLinks.map((nav) => (
+            <NavLink
+              key={nav.text.toLowerCase()}
+              to={nav.to}
+              className={({ isActive }) =>
+                `block text-white font-medium px-6 py-2 rounded-full shadow transition ${
+                  isActive
+                    ? 'bg-rose-700/80'
+                    : 'bg-stone-700/80 hover:bg-rose-800'
+                }`
+              }
+            >
+              {nav.text}
+            </NavLink>
+          ))}
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
