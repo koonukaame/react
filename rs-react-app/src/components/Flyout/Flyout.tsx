ef68f59d1acaa3c919a7c48dd3aced7891a6b0fe
koonukaame <marialezhebokova@yandex.ru>
2025-08-01 23:56:50 +0300

    feat: implement csv downloading

--- rs-react-app/src/components/Flyout/Flyout.tsx
+++ rs-react-app/src/components/Flyout/Flyout.tsx
@@ -3,14 +3,42 @@ import type { RootState } from '../../app';
 import { unselectAll } from '../../features';
 
 export const Flyout = () => {
-  const uids = useSelector((state: RootState) => state.select);
+  const characters = useSelector((state: RootState) => state.select);
   const dispatch = useDispatch();
 
-  return uids.length > 0 ? (
-    <div className="absolute left-0 bottom-0">
-      <div>{uids.length} items are selected</div>
+  function handleDownload(): string {
+    const headers = Object.keys(characters[0] || {}).join(', ');
+
+    const formattedData = characters.map((item) =>
+      Object.values(item)
+        .map((value) => (value === null ? 'Unknown' : value))
+        .join(', ')
+    );
+
+    const csv = [headers, formattedData].join('\n');
+
+    const blob = new Blob([csv], {
+      type: 'text/csv',
+    });
+
+    return URL.createObjectURL(blob);
+  }
+
+  return characters.length > 0 ? (
+    <div className="fixed left-6 bottom-6">
+      <div>
+        {`${characters.length} `}
+        {characters.length === 1 ? 'item is ' : 'items are '}selected
+      </div>
       <div className="flex gap-2 flex-col">
-        <button>Download</button>
+        <button>
+          <a
+            href={handleDownload()}
+            download={`${characters.length}_item${characters.length !== 1 ? 's' : ''}.csv`}
+          >
+            Download CSV
+          </a>
+        </button>
         <button onClick={() => dispatch(unselectAll())}>Unselect All</button>
       </div>
     </div>
