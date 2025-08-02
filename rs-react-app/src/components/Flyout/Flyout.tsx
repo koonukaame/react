478edce644219fe15fae9253b0694f126ba232a1
koonukaame <marialezhebokova@yandex.ru>
2025-08-02 15:50:21 +0300

    feat: update flyout styles

--- rs-react-app/src/components/Flyout/Flyout.tsx
+++ rs-react-app/src/components/Flyout/Flyout.tsx
@@ -1,10 +1,12 @@
 import { useDispatch, useSelector } from 'react-redux';
 import type { RootState } from '../../app';
 import { unselectAll } from '../../features';
+import { useState } from 'react';
 
 export const Flyout = () => {
   const characters = useSelector((state: RootState) => state.select);
   const dispatch = useDispatch();
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleDownload = () => {
     const headers = Object.keys(characters[0] || {}).join(', ');
@@ -25,23 +27,37 @@ export const Flyout = () => {
   };
 
   return characters.length > 0 ? (
-    <div className="fixed left-6 bottom-6" data-testid="flyout">
-      <div>
-        {`${characters.length} `}
-        {characters.length === 1 ? 'item is ' : 'items are '}selected
+    <div
+      className="fixed flex flex-col-reverse gap-3 left-6 bottom-6 rounded-xl p-3 bg-white text-white border border-gray-200"
+      data-testid="flyout"
+      onClick={() => setIsOpen(!isOpen)}
+    >
+      <div className="flex justify-center items-center gap-2">
+        <span className="text-gray-700 font-medium text-center">
+          {`${characters.length} `}
+          {characters.length === 1 ? 'item is ' : 'items are '}selected
+        </span>
+        <span className="text-sm text-gray-700">{isOpen ? '▲' : '▼'}</span>
       </div>
-      <div className="flex gap-2 flex-col">
-        <button>
+
+      {isOpen && (
+        <div className="flex gap-2 flex-col">
+          <button
+            onClick={() => dispatch(unselectAll())}
+            className="bg-gray-100 text-gray-700 text-sm font-semibold px-4 py-2 rounded-xl hover:bg-rose-200 transition"
+          >
+            Unselect All
+          </button>
           <a
             href={handleDownload()}
             download={`${characters.length}_item${characters.length !== 1 ? 's' : ''}.csv`}
             data-testid="download-file"
+            className="bg-rose-700/80 hover:bg-rose-800 text-white text-sm font-semibold px-4 py-2 rounded-xl transition text-center"
           >
             Download CSV
           </a>
-        </button>
-        <button onClick={() => dispatch(unselectAll())}>Unselect All</button>
-      </div>
+        </div>
+      )}
     </div>
   ) : null;
 };
