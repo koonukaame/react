81d4ff6cec312ff8d7676cdb6aeebe1cd73a772b
koonukaame <marialezhebokova@yandex.ru>
2025-08-02 20:21:42 +0300

    fix: error in ThemeToggle component

--- rs-react-app/src/app/providers/ThemeProvider/useTheme.tsx
+++ rs-react-app/src/app/providers/ThemeProvider/useTheme.tsx
@@ -4,5 +4,9 @@ import { ThemeContext } from './ThemeContext';
 export const useTheme = () => {
   const theme = useContext(ThemeContext);
 
+  if (!theme) {
+    throw new Error('theme is undefined');
+  }
+
   return theme;
 };
