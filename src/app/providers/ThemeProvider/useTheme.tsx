a21db402ddd123d085d33e76b72ddb7f4aeb0785
koonukaame <marialezhebokova@yandex.ru>
2025-08-05 18:49:41 +0300

    refactor: remove error throwing

--- src/app/providers/ThemeProvider/useTheme.tsx
+++ src/app/providers/ThemeProvider/useTheme.tsx
@@ -4,9 +4,5 @@ import { ThemeContext } from './ThemeContext';
 export const useTheme = () => {
   const theme = useContext(ThemeContext);
 
-  if (!theme) {
-    throw new Error('theme is undefined');
-  }
-
   return theme;
 };
