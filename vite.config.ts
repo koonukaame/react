4619cb14c91b9489be0509b826b4d4a5e4d171b5
koonukaame <marialezhebokova@yandex.ru>
2025-08-20 18:26:45 +0300

    chore: path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@app': path.resolve(__dirname, './src/app'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@entities': path.resolve(__dirname, './src/entities'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@test': path.resolve(__dirname, './src/test-utils'),
+      '@widget': path.resolve(__dirname, './src/widget'),
+    },
+  },
 });
