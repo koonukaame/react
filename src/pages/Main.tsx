929de57a99ec5f80ec5f0dd036531143337e0581
koonukaame <marialezhebokova@yandex.ru>
2025-08-21 13:50:10 +0300

    feat: Main page

---
+++ src/pages/Main.tsx
@@ -0,0 +1,33 @@
+import { Button, Modal } from '@shared';
+import { createPortal } from 'react-dom';
+import { ControlledForm, UncontrolledForm } from '@components';
+import { useState } from 'react';
+
+export const Main = () => {
+  const [showModal, setShowModal] = useState<
+    'controlled' | 'uncontrolled' | null
+  >(null);
+
+  return (
+    <main className="box-border h-screen max-w p-6 bg-neutral-900">
+      <div className="flex flex-col justify-center items-center h-full gap-5">
+        <Button
+          text="Uncontrolled Form"
+          onClick={() => setShowModal('uncontrolled')}
+        ></Button>
+        <Button
+          text="Controlled Form"
+          onClick={() => setShowModal('controlled')}
+        ></Button>
+        {showModal &&
+          createPortal(
+            <Modal>
+              {showModal === 'controlled' && <ControlledForm />}
+              {showModal === 'uncontrolled' && <UncontrolledForm />}
+            </Modal>,
+            document.body
+          )}
+      </div>
+    </main>
+  );
+};
