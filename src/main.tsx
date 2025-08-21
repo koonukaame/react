ea5f54974a6a8a1479f8b13f6ef9f7e49635bf02
koonukaame <marialezhebokova@yandex.ru>
2025-08-21 13:49:47 +0300

    refactor: main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
+import { App } from '@app';
 
-createRoot(document.getElementById('root')!).render(
+const root = document.getElementById('root');
+
+if (!root) {
+  throw new Error('root not found');
+}
+
+createRoot(root).render(
   <StrictMode>
     <App />
   </StrictMode>
