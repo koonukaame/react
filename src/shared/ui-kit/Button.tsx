59d204b47831c8b12f206c2adbf6b32ece8df978
koonukaame <marialezhebokova@yandex.ru>
2025-08-22 16:02:22 +0300

    refactor: update Button

--- src/shared/ui-kit/Button.tsx
+++ src/shared/ui-kit/Button.tsx
@@ -1,13 +1,20 @@
+import { cn } from '../utils';
+
 type Props = {
   text: string;
   onClick: () => void;
+  isDanger?: boolean;
 };
 
-export const Button = ({ text, onClick }: Props) => {
+export const Button = ({ text, onClick, isDanger }: Props) => {
   return (
     <button
-      className="bg-violet-500/40 py-3 rounded-lg text-xl min-w-[300px] text-indigo-50 cursor-pointer 
-      transition duration-200 hover:bg-violet-500/50 hover:-translate-y-0.5 active:translate-y-0 active:bg-violet-500/60"
+      className={cn(
+        'py-3 rounded-lg text-xl min-w-[300px] text-indigo-50 cursor-pointer transition duration-200 mt-0',
+        isDanger
+          ? 'bg-red-500/40 hover:bg-red-500/50 active:bg-red-500/60'
+          : 'bg-violet-500/40 hover:bg-violet-500/50 active:bg-violet-500/60'
+      )}
       onClick={onClick}
     >
       {text}
