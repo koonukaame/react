c2f66da4d139a23fe02810dcd7e0de1ba2c93787
koonukaame <marialezhebokova@yandex.ru>
2025-08-22 15:57:45 +0300

    refactor: update modal

--- src/shared/ui-kit/Modal.tsx
+++ src/shared/ui-kit/Modal.tsx
@@ -1,9 +1,30 @@
-import type { ReactNode } from 'react';
+import { useEffect, useRef, type ReactNode } from 'react';
 
 type Props = {
   children: ReactNode;
+  onClose: () => void;
 };
 
-export const Modal = ({ children }: Props) => {
-  return <dialog>{children}</dialog>;
+export const Modal = ({ children, onClose }: Props) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    ref.current?.focus();
+  }, []);
+
+  return (
+    <div
+      className="fixed inset-0 flex items-center justify-center"
+      onClick={onClose}
+      onKeyDown={(e) => {
+        if (e.key === 'Escape') {
+          onClose();
+        }
+      }}
+      tabIndex={-1}
+      ref={ref}
+    >
+      <div onClick={(e) => e.stopPropagation()}>{children}</div>
+    </div>
+  );
 };
