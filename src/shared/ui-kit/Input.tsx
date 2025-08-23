8935ff658d06b982c8574a9c5ad1fd4823765147
koonukaame <marialezhebokova@yandex.ru>
2025-08-23 18:27:13 +0300

    refactor: update Input component

--- src/shared/ui-kit/Input.tsx
+++ src/shared/ui-kit/Input.tsx
@@ -3,16 +3,25 @@ import type { InputHTMLAttributes } from 'react';
 type Props = {
   isControlled: boolean;
   onChange?: (value: string) => void;
+  error?: string;
 } & InputHTMLAttributes<HTMLInputElement>;
 
-export const Input = ({ onChange, isControlled, ...rest }: Props) => {
-  return isControlled ? (
-    <input
-      onChange={onChange ? (e) => onChange(e.target.value) : undefined}
-      className="p-2 border border-neutral-700"
-      {...rest}
-    />
-  ) : (
-    <input {...rest} className="p-2 border border-neutral-700" />
+export const Input = ({ onChange, isControlled, error, ...rest }: Props) => {
+  return (
+    <div>
+      {isControlled ? (
+        <input
+          onChange={onChange ? (e) => onChange(e.target.value) : undefined}
+          className="p-2 border border-neutral-700 w-full rounded-md"
+          {...rest}
+        />
+      ) : (
+        <input
+          {...rest}
+          className="p-2 border border-neutral-700 w-full rounded-md"
+        />
+      )}
+      <div className="text-red-500/90 h-5">{error}</div>
+    </div>
   );
 };
