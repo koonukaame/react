285bf0dd22d2504f1e13835f37a6d23030a22ad0
koonukaame <marialezhebokova@yandex.ru>
2025-08-24 12:22:18 +0300

    feat: countries slice & Datalist component

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,1 +1,2 @@
+export { countriesSlice } from './countriesSlice';
 export { addForm, formsSlice, type FormData } from './formsSlice';
---
+++ src/shared/ui-kit/Datalist.tsx
@@ -0,0 +1,29 @@
+import type { InputHTMLAttributes } from 'react';
+import { useMainSelector } from '../hooks';
+
+type Props = {
+  label: string;
+  id: string;
+} & InputHTMLAttributes<HTMLInputElement>;
+
+export const Datalist = ({ label, id, ...rest }: Props) => {
+  const countries = useMainSelector((state) => state.countries);
+
+  return (
+    <div className="w-full">
+      <label htmlFor={id}>{label}</label>
+      <input
+        list={`${id}-list`}
+        id={id}
+        name={id}
+        {...rest}
+        className="w-full p-2 border border-neutral-700 rounded-md text-white"
+      />
+      <datalist id={`${id}-list`}>
+        {countries.map((country) => (
+          <option key={country} value={country} />
+        ))}
+      </datalist>
+    </div>
+  );
+};
