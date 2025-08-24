6f00c25f061e8f5b7bc24b2df30667baba07945f
koonukaame <marialezhebokova@yandex.ru>
2025-08-24 19:29:14 +0300

    chore: vitest, rts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
@@ -6,6 +7,27 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{js,jsx,ts,tsx}',
+        'src/index.{js,jsx,ts,tsx}',
+        'src/**/*.d.ts',
+        'src/**/*index.{js,jsx,ts,tsx}',
+      ],
+      thresholds: {
+        global: {
+          statements: 80,
+          branches: 50,
+          functions: 50,
+          lines: 50,
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       '@app': path.resolve(__dirname, './src/app'),
@@ -16,6 +38,11 @@ export default defineConfig({
       '@shared': path.resolve(__dirname, './src/shared'),
       '@test': path.resolve(__dirname, './src/test-utils'),
       '@widget': path.resolve(__dirname, './src/widget'),
+    },
+  },
+  server: {
+    watch: {
+      ignored: ['**/coverage/**'],
     },
   },
 });
